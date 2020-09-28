b8e4494d0f573775cc0d0591b02b18a945a268a4
Yan Jiang <yanjiang@Yans-MacBook-Air.local>
2020-09-28 15:05:37 +0200

    fix

--- src/component/Form.js
+++ src/component/Form.js
@@ -6,9 +6,9 @@ function Form() {
         src="https://docs.google.com/forms/d/e/1FAIpQLSc2tz74zJwsT-e8gziiFsWDpLsqNyhfNySL4mwb3SOzLlplzg/viewform?embedded=true"
         width="640"
         height="504"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
       >
         Loading…
       </iframe>
