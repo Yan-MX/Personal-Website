91e1eafe81faf97cc47a59b30a5df738d39db134
Yan Jiang <yanjiang@Yans-MacBook-Air.local>
2020-10-07 11:40:54 +0200

    add one more project

--- src/component/Data.js
+++ src/component/Data.js
@@ -4,6 +4,7 @@ let data = [
     name: "Yan's E-commerce",
     git: "https://github.com/Yan-MX/E-commerce",
     website: "http://yan-ecommerce.herokuapp.com/",
+    page: "",
     pic: "https://iili.io/2c3gEB.png",
 
     description:
@@ -14,10 +15,22 @@ let data = [
     name: "H2O studio Website",
     git: "https://github.com/Yan-MX/H2O-studio-website",
     website: "http://h2o-studio.herokuapp.com/",
+    page: "",
     pic: "https://iili.io/2cFQS9.png",
 
     description:
       "A informative, interactive website for a street dance studio. The website includes a landing page and a register page.",
+  },
+  {
+    id: 3,
+    name: "Bored No More App",
+    git: "https://github.com/Yan-MX/BoredNoMore",
+    website: "",
+    page: "https://expo.io/@yannay/projects/BoredNoMore",
+    pic: "https://iili.io/2S5xAF.png",
+
+    description:
+      "A react native app that can offer you some ideas of what to do when you feel bored at home. By spinning the magic wheels, you will get some inspirations. Built with react native Gesture Handler, Svg and Animation",
   },
 ];
 export default data;
--- src/component/Project.js
+++ src/component/Project.js
@@ -2,10 +2,12 @@ import React from "react";
 import data from "./Data";
 import styled from "@emotion/styled";
 function Project() {
+  // styling
   let Button = styled.button`
     background-color: #d43109;
     padding: 0.5vw;
-    width: 10vw;
+    width: 8vw;
+    margin-right: 1vw;
   `;
   let Title = styled.p`
     margin: 5vw 0;
@@ -13,7 +15,7 @@ function Project() {
   let Button2 = styled.button`
     background-color: #4f8a8b;
     padding: 0.5vw;
-    width: 10vw;
+    width: 8vw;
   `;
   let Figure = styled.figure`
     position: relative;
@@ -51,7 +53,7 @@ function Project() {
     padding: 0;
     margin: 0;
   `;
-
+  // use data from data.js to display each project
   return (
     <div>
       <Title>My Projects</Title>
@@ -71,10 +73,12 @@ function Project() {
                       type="button"
                       onClick={(e) => {
                         e.preventDefault();
-                        window.open(project.website);
+                        project.website
+                          ? window.open(project.website)
+                          : window.open(project.page);
                       }}
                     >
-                      Website
+                      {project.website ? "Website" : "Page"}
                     </Button>
                     <Button2
                       type="button"
